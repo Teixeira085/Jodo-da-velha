69cf2cc85ab9ae5b3990185b0321f85b3fb922b2
Rafael teixeira <Teixeiraflavia842@gmail.com>
2025-05-19 15:05:46 -0300

    Initial commit

---
+++ Jodo da velha/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Jodo_da_velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pibResetar = new System.Windows.Forms.PictureBox();
+            this.pibFechar = new System.Windows.Forms.PictureBox();
+            this.pibTicTac = new System.Windows.Forms.PictureBox();
+            this.pibJogo = new System.Windows.Forms.PictureBox();
+            this.btn1_1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pibResetar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibFechar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibTicTac)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibJogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pibResetar
+            // 
+            this.pibResetar.Image = global::Jodo_da_velha.Properties.Resources._1564533_arrow_refrech_reload_update_icon;
+            this.pibResetar.Location = new System.Drawing.Point(477, 315);
+            this.pibResetar.Name = "pibResetar";
+            this.pibResetar.Size = new System.Drawing.Size(100, 50);
+            this.pibResetar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibResetar.TabIndex = 5;
+            this.pibResetar.TabStop = false;
+            // 
+            // pibFechar
+            // 
+            this.pibFechar.Image = global::Jodo_da_velha.Properties.Resources.multiply;
+            this.pibFechar.Location = new System.Drawing.Point(477, 238);
+            this.pibFechar.Name = "pibFechar";
+            this.pibFechar.Size = new System.Drawing.Size(100, 50);
+            this.pibFechar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibFechar.TabIndex = 4;
+            this.pibFechar.TabStop = false;
+            this.pibFechar.Click += new System.EventHandler(this.picFechar_Click);
+            // 
+            // pibTicTac
+            // 
+            this.pibTicTac.Image = global::Jodo_da_velha.Properties.Resources.tttmarqlrg;
+            this.pibTicTac.Location = new System.Drawing.Point(3, 12);
+            this.pibTicTac.Name = "pibTicTac";
+            this.pibTicTac.Size = new System.Drawing.Size(574, 126);
+            this.pibTicTac.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibTicTac.TabIndex = 3;
+            this.pibTicTac.TabStop = false;
+            // 
+            // pibJogo
+            // 
+            this.pibJogo.Image = global::Jodo_da_velha.Properties.Resources._tictactoe__1_;
+            this.pibJogo.Location = new System.Drawing.Point(3, 144);
+            this.pibJogo.Name = "pibJogo";
+            this.pibJogo.Size = new System.Drawing.Size(468, 468);
+            this.pibJogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pibJogo.TabIndex = 1;
+            this.pibJogo.TabStop = false;
+            // 
+            // btn1_1
+            // 
+            this.btn1_1.BackColor = System.Drawing.Color.White;
+            this.btn1_1.Location = new System.Drawing.Point(36, 193);
+            this.btn1_1.Name = "btn1_1";
+            this.btn1_1.Size = new System.Drawing.Size(113, 95);
+            this.btn1_1.TabIndex = 6;
+            this.btn1_1.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(180, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 95);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(335, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 95);
+            this.button2.TabIndex = 8;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(36, 339);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 95);
+            this.button3.TabIndex = 9;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(325, 339);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 95);
+            this.button4.TabIndex = 9;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(180, 339);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 95);
+            this.button5.TabIndex = 10;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(36, 484);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(113, 95);
+            this.button6.TabIndex = 11;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(180, 484);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(113, 95);
+            this.button8.TabIndex = 12;
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(325, 484);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(113, 95);
+            this.button9.TabIndex = 13;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(31, 618);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(132, 29);
+            this.lblInfo.TabIndex = 14;
+            this.lblInfo.Text = "É a vez do X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 656);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn1_1);
+            this.Controls.Add(this.pibResetar);
+            this.Controls.Add(this.pibFechar);
+            this.Controls.Add(this.pibTicTac);
+            this.Controls.Add(this.pibJogo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pibResetar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibFechar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibTicTac)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibJogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pibJogo;
+        private System.Windows.Forms.PictureBox pibTicTac;
+        private System.Windows.Forms.PictureBox pibFechar;
+        private System.Windows.Forms.PictureBox pibResetar;
+        private System.Windows.Forms.Button btn1_1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
