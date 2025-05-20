5ad594530ed10eed232c09a9f90441233a333a97
Rafael teixeira <Teixeiraflavia842@gmail.com>
2025-05-20 13:39:10 -0300

    jogo da velha

--- Jodo da velha/Form1.Designer.cs
+++ Jodo da velha/Form1.Designer.cs
@@ -31,17 +31,17 @@
             this.pibResetar = new System.Windows.Forms.PictureBox();
             this.pibFechar = new System.Windows.Forms.PictureBox();
             this.pibTicTac = new System.Windows.Forms.PictureBox();
-            this.pibJogo = new System.Windows.Forms.PictureBox();
-            this.btn1_1 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
             this.lblInfo = new System.Windows.Forms.Label();
+            this.pibJogo = new System.Windows.Forms.PictureBox();
+            this.btn23 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn31 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn32 = new System.Windows.Forms.Button();
+            this.btn33 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pibResetar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pibFechar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pibTicTac)).BeginInit();
@@ -57,6 +57,7 @@
             this.pibResetar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pibResetar.TabIndex = 5;
             this.pibResetar.TabStop = false;
+            this.pibResetar.Click += new System.EventHandler(this.pibResetar_Click);
             // 
             // pibFechar
             // 
@@ -79,6 +80,50 @@
             this.pibTicTac.TabIndex = 3;
             this.pibTicTac.TabStop = false;
             // 
+            // btn11
+            // 
+            this.btn11.BackColor = System.Drawing.Color.White;
+            this.btn11.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn11.Location = new System.Drawing.Point(36, 193);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(113, 95);
+            this.btn11.TabIndex = 6;
+            this.btn11.UseVisualStyleBackColor = false;
+            this.btn11.Click += new System.EventHandler(this.Jogada_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.BackColor = System.Drawing.Color.White;
+            this.btn12.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn12.Location = new System.Drawing.Point(180, 193);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(113, 95);
+            this.btn12.TabIndex = 7;
+            this.btn12.UseVisualStyleBackColor = false;
+            this.btn12.Click += new System.EventHandler(this.Jogada_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.BackColor = System.Drawing.Color.White;
+            this.btn13.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn13.Location = new System.Drawing.Point(325, 193);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(113, 95);
+            this.btn13.TabIndex = 8;
+            this.btn13.UseVisualStyleBackColor = false;
+            this.btn13.Click += new System.EventHandler(this.Jogada_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.BackColor = System.Drawing.Color.Red;
+            this.lblInfo.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(31, 618);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(132, 29);
+            this.lblInfo.TabIndex = 14;
+            this.lblInfo.Text = "É a vez do X";
+            // 
             // pibJogo
             // 
             this.pibJogo.Image = global::Jodo_da_velha.Properties.Resources._tictactoe__1_;
@@ -89,112 +134,87 @@
             this.pibJogo.TabIndex = 1;
             this.pibJogo.TabStop = false;
             // 
-            // btn1_1
+            // btn23
             // 
-            this.btn1_1.BackColor = System.Drawing.Color.White;
-            this.btn1_1.Location = new System.Drawing.Point(36, 193);
-            this.btn1_1.Name = "btn1_1";
-            this.btn1_1.Size = new System.Drawing.Size(113, 95);
-            this.btn1_1.TabIndex = 6;
-            this.btn1_1.UseVisualStyleBackColor = false;
+            this.btn23.BackColor = System.Drawing.Color.White;
+            this.btn23.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn23.Location = new System.Drawing.Point(325, 327);
+            this.btn23.Name = "btn23";
+            this.btn23.Size = new System.Drawing.Size(113, 95);
+            this.btn23.TabIndex = 15;
+            this.btn23.UseVisualStyleBackColor = false;
+            this.btn23.Click += new System.EventHandler(this.Jogada_Click);
             // 
-            // button1
+            // btn22
             // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(180, 193);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 95);
-            this.button1.TabIndex = 7;
-            this.button1.UseVisualStyleBackColor = false;
+            this.btn22.BackColor = System.Drawing.Color.White;
+            this.btn22.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn22.Location = new System.Drawing.Point(180, 327);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(113, 95);
+            this.btn22.TabIndex = 16;
+            this.btn22.UseVisualStyleBackColor = false;
+            this.btn22.Click += new System.EventHandler(this.Jogada_Click);
             // 
-            // button2
+            // btn31
             // 
-            this.button2.BackColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(335, 205);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 95);
-            this.button2.TabIndex = 8;
-            this.button2.UseVisualStyleBackColor = false;
+            this.btn31.BackColor = System.Drawing.Color.White;
+            this.btn31.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn31.Location = new System.Drawing.Point(36, 479);
+            this.btn31.Name = "btn31";
+            this.btn31.Size = new System.Drawing.Size(113, 95);
+            this.btn31.TabIndex = 17;
+            this.btn31.UseVisualStyleBackColor = false;
+            this.btn31.Click += new System.EventHandler(this.Jogada_Click);
             // 
-            // button3
+            // btn21
             // 
-            this.button3.BackColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(36, 339);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(113, 95);
-            this.button3.TabIndex = 9;
-            this.button3.UseVisualStyleBackColor = false;
+            this.btn21.BackColor = System.Drawing.Color.White;
+            this.btn21.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn21.Location = new System.Drawing.Point(36, 327);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(113, 95);
+            this.btn21.TabIndex = 18;
+            this.btn21.UseVisualStyleBackColor = false;
+            this.btn21.Click += new System.EventHandler(this.Jogada_Click);
             // 
-            // button4
+            // btn32
             // 
-            this.button4.BackColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(325, 339);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(113, 95);
-            this.button4.TabIndex = 9;
-            this.button4.UseVisualStyleBackColor = false;
+            this.btn32.BackColor = System.Drawing.Color.White;
+            this.btn32.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn32.Location = new System.Drawing.Point(180, 479);
+            this.btn32.Name = "btn32";
+            this.btn32.Size = new System.Drawing.Size(113, 95);
+            this.btn32.TabIndex = 19;
+            this.btn32.UseVisualStyleBackColor = false;
+            this.btn32.Click += new System.EventHandler(this.Jogada_Click);
             // 
-            // button5
+            // btn33
             // 
-            this.button5.BackColor = System.Drawing.Color.White;
-            this.button5.Location = new System.Drawing.Point(180, 339);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(113, 95);
-            this.button5.TabIndex = 10;
-            this.button5.UseVisualStyleBackColor = false;
-            // 
-            // button6
-            // 
-            this.button6.BackColor = System.Drawing.Color.White;
-            this.button6.Location = new System.Drawing.Point(36, 484);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(113, 95);
-            this.button6.TabIndex = 11;
-            this.button6.UseVisualStyleBackColor = false;
-            // 
-            // button8
-            // 
-            this.button8.BackColor = System.Drawing.Color.White;
-            this.button8.Location = new System.Drawing.Point(180, 484);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(113, 95);
-            this.button8.TabIndex = 12;
-            this.button8.UseVisualStyleBackColor = false;
-            // 
-            // button9
-            // 
-            this.button9.BackColor = System.Drawing.Color.White;
-            this.button9.Location = new System.Drawing.Point(325, 484);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(113, 95);
-            this.button9.TabIndex = 13;
-            this.button9.UseVisualStyleBackColor = false;
-            // 
-            // lblInfo
-            // 
-            this.lblInfo.AutoSize = true;
-            this.lblInfo.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblInfo.Location = new System.Drawing.Point(31, 618);
-            this.lblInfo.Name = "lblInfo";
-            this.lblInfo.Size = new System.Drawing.Size(132, 29);
-            this.lblInfo.TabIndex = 14;
-            this.lblInfo.Text = "É a vez do X";
+            this.btn33.BackColor = System.Drawing.Color.White;
+            this.btn33.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn33.Location = new System.Drawing.Point(325, 479);
+            this.btn33.Name = "btn33";
+            this.btn33.Size = new System.Drawing.Size(113, 95);
+            this.btn33.TabIndex = 20;
+            this.btn33.UseVisualStyleBackColor = false;
+            this.btn33.Click += new System.EventHandler(this.Jogada_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(589, 656);
+            this.Controls.Add(this.btn33);
+            this.Controls.Add(this.btn32);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn31);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn23);
             this.Controls.Add(this.lblInfo);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btn1_1);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
             this.Controls.Add(this.pibResetar);
             this.Controls.Add(this.pibFechar);
             this.Controls.Add(this.pibTicTac);
@@ -211,20 +231,20 @@
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pibJogo;
         private System.Windows.Forms.PictureBox pibTicTac;
         private System.Windows.Forms.PictureBox pibFechar;
         private System.Windows.Forms.PictureBox pibResetar;
-        private System.Windows.Forms.Button btn1_1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn13;
         private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.PictureBox pibJogo;
+        private System.Windows.Forms.Button btn23;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn31;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn32;
+        private System.Windows.Forms.Button btn33;
     }
 }
 
--- Jodo da velha/Form1.cs
+++ Jodo da velha/Form1.cs
@@ -12,6 +12,9 @@ namespace Jodo_da_velha
 {
     public partial class Form1 : Form
     {
+        //váriaveis globais: 
+        string jogadaAtual = "O";
+
         public Form1()
         {
             InitializeComponent();
@@ -22,5 +25,147 @@ namespace Jodo_da_velha
             Application.Exit(); //fechar o programa
 
         }
+
+        private void Jogada_Click(object sender, EventArgs e)
+        {
+            Button botaoClicado = (Button)sender;
+            botaoClicado.Text = jogadaAtual;
+            botaoClicado.Enabled = false;
+            // trocar de x para O e de O paraX X: 
+            if (jogadaAtual == "X")
+            {
+                jogadaAtual = "O";
+                // torcar a cor do label: 
+                lblInfo.ForeColor = Color.Green;
+
+            }
+            else
+            {
+                jogadaAtual = "X"; 
+                // trocar a cor do label: 
+                lblInfo.ForeColor = Color.Red;
+            }
+
+            // Atualizar p label: 
+            lblInfo.Text = $"É a vez do {jogadaAtual}";
+
+            //Vereficar o vencedor 
+            VerificarVencedor();
+        }
+
+        private void pibResetar_Click(object sender, EventArgs e)
+        {
+            jogadaAtual = "x";
+            lblInfo.Text = $"É a vez do {jogadaAtual}";
+            lblInfo.ForeColor = Color.Red ;
+
+            //Resetar os botões :
+            btn11.Enabled = true;
+            btn11.Text = "";
+
+            btn12.Enabled = true;
+            btn12.Text = "";
+
+            btn13.Enabled = true;
+            btn13.Text = "";
+
+            btn21.Enabled = true;
+            btn22.Text  = "";
+
+            btn22.Enabled = true;
+            btn22.Text = "";
+
+            btn23.Enabled = true;
+            btn23.Text = "";
+
+            btn31.Enabled = true;
+            btn31.Text = "";
+
+            btn32.Enabled = true;
+            btn32.Text = "";
+
+            btn33.Enabled = true;
+            btn33.Text = "";
+
+
+
+
+        }
+        public void VerificarVencedor()
+        {
+            // Verificar linhas:
+            if (btn11.Text == btn12.Text && btn12.Text == btn13.Text && btn11.Text != "")
+            {
+                /* ---
+                 * zzz
+                 * zzz
+                 */
+                lblInfo.Text = $"O vencedor é {btn11.Text}";
+            }
+            else if (btn21.Text == btn22.Text && btn22.Text == btn23.Text && btn21.Text != "")
+            {
+                /* zzz
+                 * ---
+                 * zzz
+                 */
+                lblInfo.Text = $"O vencedor é {btn21.Text}";
+            }
+            else if (btn31.Text == btn32.Text && btn32.Text == btn33.Text && btn31.Text != "")
+            {
+                /* zzz
+                 * zzz
+                 * ---
+                 */
+                lblInfo.Text = $"O vencedor é {btn31.Text}";
+            }
+            else if (btn11.Text == btn21.Text && btn21.Text == btn31.Text && btn11.Text != "")
+            {
+                /* -zz
+                 * -zz
+                 * -zz
+                 */
+                lblInfo.Text = $"O vencedor é {btn11.Text}";
+            }
+            else if (btn12.Text == btn22.Text && btn22.Text == btn32.Text && btn12.Text != "")
+            {
+                /* z-z
+                 * z-z
+                 * z-z
+                 */
+                lblInfo.Text = $"O vencedor é {btn12.Text}";
+            }
+            else if (btn13.Text == btn23.Text && btn23.Text == btn33.Text && btn13.Text != "")
+            {
+                /* zz-
+                 * zz-
+                 * zz-
+                 */
+                lblInfo.Text = $"O vencedor é {btn13.Text}";
+            }
+            else if (btn11.Text == btn22.Text && btn22.Text == btn33.Text && btn11.Text != "")
+            {
+                /* -zz
+                 * z-z
+                 * zz-
+                 */
+                lblInfo.Text = $"O vencedor é {btn11.Text}";
+            }
+            else if (btn13.Text == btn22.Text && btn22.Text == btn31.Text && btn13.Text != "")
+            {
+                /* zz-
+                 * z-z
+                 * -zz
+                 */
+                lblInfo.Text =s
+        }
+
+
+
+
+
+
     }
-}
+
+        }
+    
+
